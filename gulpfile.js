73f2bc97752381078faca0cebf27779022381425
Hercules Lin <sdsg5bpnl@outlook.com>
2019-10-04 12:52:57 +0800

    調整 gulp 流程，修復不會自動檢查 views 狀態更新的錯誤

--- gulpfile.js
+++ gulpfile.js
@@ -27,10 +27,7 @@ const options = parseArgs(process.argv.slice(2), envOptions);
 function copyHTML() {
   return src('src/*.html')
     .pipe(
-      gulpif(
-        options.env === 'production',
-        htmlmin({ collapseWhitespace: true, removeComments: true }),
-      ),
+      gulpif(options.env === 'production', htmlmin({ removeComments: true })),
     )
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
@@ -38,7 +35,7 @@ function copyHTML() {
 
 function pugToHTML() {
   return src('src/*.pug')
-    .pipe(gulpif(options.env === 'production', pug({}), pug({ pretty: true })))
+    .pipe(pug({ pretty: true }))
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
 }
@@ -48,10 +45,7 @@ function ejsToHTML() {
     .pipe(ejs({}))
     .pipe(rename({ extname: '.html' }))
     .pipe(
-      gulpif(
-        options.env === 'production',
-        htmlmin({ collapseWhitespace: true, removeComments: true }),
-      ),
+      gulpif(options.env === 'production', htmlmin({ removeComments: true })),
     )
     .pipe(dest('dist'))
     .pipe(browserSync.stream());
@@ -59,17 +53,9 @@ function ejsToHTML() {
 
 function copyCSS() {
   return src('src/styles/**/*.css')
-    .pipe(gulpif(options.env === 'production', sourcemaps.init()))
-    .pipe(
-      gulpif(
-        options.env === 'production',
-        cleanCSS({ compatibility: 'ie8', debug: true }, details => {
-          console.log(`${details.name}: ${details.stats.originalSize}`);
-          console.log(`${details.name}: ${details.stats.minifiedSize}`);
-        }),
-      ),
-    )
-    .pipe(gulpif(options.env === 'production', sourcemaps.write('.')))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('dist/styles'))
     .pipe(browserSync.stream());
 }
@@ -78,16 +64,9 @@ function scssToCSS() {
   return src('src/styles/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(
-      gulpif(
-        options.env === 'production',
-        dartSass({
-          outputStyle: 'compressed',
-          includePaths: ['./node_modules/bootstrap/scss'],
-        }).on('error', dartSass.logError),
-        dartSass({
-          includePaths: ['./node_modules/bootstrap/scss'],
-        }).on('error', dartSass.logError),
-      ),
+      dartSass({
+        includePaths: ['./node_modules/bootstrap/scss'],
+      }).on('error', dartSass.logError),
     )
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
@@ -143,8 +122,8 @@ function serve() {
     },
   });
   watch('src/*.html', series(copyHTML));
-  watch('src/*.pug', series(pugToHTML));
-  watch('src/*.ejs', series(ejsToHTML));
+  watch('src/**/*.pug', series(pugToHTML));
+  watch('src/**/*.ejs', series(ejsToHTML));
   watch('src/styles/**/*.css', series(copyCSS));
   watch('src/styles/**/*.scss', series(scssToCSS));
   watch('src/scripts/**/*.js', series(copyJS));
